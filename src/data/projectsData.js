3e4e00819ff0634b3f11aec3e3657cc7abd0ba8a
bno4 <bnduchemin@gmail.com>
2023-12-07 10:28:48 +0100

    màj de la base de données

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,9 +3,9 @@ export const projectsData = [
     id: 1,
     title: "Kasa",
     date: "Juin 2023",
-    languages: ["React", "Sass"],
+    languages: ["REACT", "SASS"],
     infos:
-      "Création de la partie front-end avec React et SASS pour le style. Contrainte de l'exercice : aucune bibliothèque extérieure à React n'a été autorisée. Maquette Dektop et mobile © OpenClassroom.",
+      "Création de la partie front-end avec React et SASS pour le style. Aucune bibliothèque extérieure à React n'a été utilisée. Maquette Dektop et mobile © OpenClassroom.",
     img: "./assets/img/kasa_home.webp",
     link: "https://bno4.github.io/kasa",
     git: "https://github.com/bno4/kasa",
@@ -14,7 +14,7 @@ export const projectsData = [
     id: 2,
     title: "Kanap",
     date: "Mars 2023",
-    languages: ["Vanilla JS "],
+    languages: ["JAVASCRIPT"],
     infos:
       "Création de la partie front d'un site e-commerce en Javascript. L'application permet d'ajouter un produit, sélectionner la couleur et le nombre de produit. Modification possible jusqu'à la confirmation de commande",
     img: "./assets/img/kanap_home.webp",
@@ -25,9 +25,9 @@ export const projectsData = [
     id: 3,
     title: "OhMyFood",
     date: "Février 2023",
-    languages: ["Sass"],
+    languages: ["SASS"],
     infos:
-      "Site de restaurant 'mobile first', Création de l'html et du style avec Sass. Maquette dektop et mobile © OpenClassroom",
+      "Site de restaurant 'mobile first', Création de l'html et du style avec Sass. Maquette desktop et mobile © OpenClassroom",
     img: "./assets/img/ohmyfood_home.webp",
     link: "https://bno4.github.io/projet3",
     git: "https://github.com/bno4/projet3",
@@ -38,7 +38,7 @@ export const projectsData = [
     date: "Avril 2023",
     languages: ["Node"],
     infos:
-      "Création du back d'un application de post et notation de sauces piquantes avec Node, Express et MongoDB. Partie Front © OpenClassrooms",
+      "Création du back d'une application de post et notation de sauces piquantes avec Node, Express et MongoDB. Partie Front © OpenClassrooms",
     img: "./assets/img/hottakes_home.webp",
     link: "",
     git: "https://github.com/bno4/piiquante",
@@ -47,11 +47,11 @@ export const projectsData = [
     id: 5,
     title: "La Panthère",
     date: "Février 2023",
-    languages: ["SEO", "Css", "Html"],
+    languages: ["SEO", "CSS", "HTML"],
     infos:
-      "Site factice d'agence web optimisé pour le référencement et l'accessibilité. Le site originel est livé par OpenClassrooms. Optimisation faite au niveau du Html et Css (meta description, ajusement des contrastes, responsive, etc.",
+      "Site factice d'agence web optimisé pour le référencement et l'accessibilité (SEO). Le site originel est livré par OpenClassrooms. Optimisations apportées au niveau du Html et Css (meta description, ajusement des contrastes, site responsive, réduction du poids des photos, etc.). D'après PageSpeed Insights, le site atteint 99% en performances (94% sur mobile), 100 % sur l'accessibilité, 100% sur le SEO et 100% pour les bonnes pratiques",
     img: "./assets/img/lapanthere_home.webp",
-    link: "http://www.google.com",
+    link: "https://bno4.github.io/la-panthere",
   },
   {
     id: 6,
